da7ffc683691c0860de1d113ee99a6c224f69cbd
Suman More <67056908+SumanMore@users.noreply.github.com>
2020-07-13 10:42:33 +0530

    Add files via upload

---
+++ JAVA/Java_Loops_II.java
@@ -0,0 +1,25 @@
+package hackerrank;
+import java.util.*;
+import java.io.*;
+
+class  Java_Loops_II{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int s=0;
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            s=a+(int)(Math.pow(2,0)*b);
+              System.out.print(s+" ");
+            for (int x=2;x<=n;x++)
+            {s=s+(int)(Math.pow(2,x-1)*b);
+            System.out.print(s+" ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
