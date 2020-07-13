d45e2665b14dfdee174b186c02d3bc1cbec7a965
Suman More <67056908+SumanMore@users.noreply.github.com>
2020-07-13 09:43:19 +0530

    Update Java String Reverse.java

--- JAVA/Java String Reverse.java
+++ JAVA/Java String Reverse.java
@@ -1,1 +1,33 @@
 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        
+        /* Enter your code here. Print output to STDOUT. */
+        String s="";
+       for(int i=0;i<A.length();i++)
+       {
+           char ch=A.charAt(i);
+           s=ch+s;
+           
+       }
+       if(A.equals(s))
+       {
+           System.out.println("Yes");
+       }
+       else 
+       {
+           System.out.println("No");
+       }
+        
+    }
+}
+
+
+
